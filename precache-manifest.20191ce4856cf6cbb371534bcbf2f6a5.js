51977279c36b2424dc80eb2576798e62b488eed2
Hồ Ngọc Thanh Tâm <tam.ho@student.passerellesnumeriques.org>
2019-05-31 11:11:56 +0700

    Updates

---
+++ precache-manifest.20191ce4856cf6cbb371534bcbf2f6a5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a235a3e471c6506a03305655b06730a",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "d143914a89f6e7623b99",
+    "url": "/react-test/static/css/main.a1785866.chunk.css"
+  },
+  {
+    "revision": "eac7b8b4023f686e34b3",
+    "url": "/react-test/static/js/2.4b6b28c6.chunk.js"
+  },
+  {
+    "revision": "d143914a89f6e7623b99",
+    "url": "/react-test/static/js/main.ccea47f4.chunk.js"
+  },
+  {
+    "revision": "91e87af5d0bfc2a79f8e",
+    "url": "/react-test/static/js/runtime~main.b9ad4e01.js"
+  },
+  {
+    "revision": "e9f5edd1b0d682bed17d922857d9b60c",
+    "url": "/react-test/static/media/changeIcon.e9f5edd1.jpg"
+  },
+  {
+    "revision": "9f74f076a9ea74ab6053903f905222c7",
+    "url": "/react-test/static/media/circle_icon.9f74f076.png"
+  },
+  {
+    "revision": "c084617529a528f41eb1dd550123116c",
+    "url": "/react-test/static/media/github_icon.c0846175.png"
+  }
+]);
